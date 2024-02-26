faa76dd5f8853fc2cf732cf80af206d4aa57ae06
picoting <lindsayl.6857@gmail.com>
2024-02-26 14:10:42 -0500

    added boolean requiresPolice to crime class

--- 10. Displaying Lists with RecyclerView/Solution/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ 10. Displaying Lists with RecyclerView/Solution/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -1,11 +1,12 @@
 package com.bignerdranch.android.criminalintent
 
-import java.util.UUID
 import java.util.Date
+import java.util.UUID
 
 data class Crime(
     val id: UUID,
     val title: String,
     val date: Date,
-    val isSolved: Boolean
+    val isSolved: Boolean,
+    val requiresPolice: Boolean
 )
